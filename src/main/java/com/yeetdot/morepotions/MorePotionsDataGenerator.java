9665bab9dde3fa390a72b1148744e65e70cfa62f
Minhao Chen <randomrepl@gmail.com>
2024-08-26 10:11:21 -0400

    Initial Commit

---
+++ src/main/java/com/yeetdot/morepotions/MorePotionsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.yeetdot.morepotions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MorePotionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
